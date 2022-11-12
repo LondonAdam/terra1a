cd55edc0ee4cc995b091a24b3c67ae8a70e4e87a
LondonAdam <adambartlett333@gmail.com>
2022-11-12 15:59:54 +0000

    make an AWS instance in Dublin

---
+++ crtAWS1.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  access_key = "AKIAUNXKZQLSBIH456CA"
+  secret_key = "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"
+  region = "eu-west-1"
+}
+
+resource "aws_instance" "niceT1" {
+  ami           = "ami-0da83231e619d28c4"
+  instance_type = "t2.micro"
+}
