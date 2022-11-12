6adca5d39aa6bfc1a6825ab62a87a3bae07e53a9
LondonAdam <adambartlett333@gmail.com>
2022-11-12 18:37:14 +0000

    remove secrets!

--- crtAWS1.tf
+++ crtAWS1.tf
@@ -1,6 +1,4 @@
 provider "aws" {
-  access_key = "AKIAUNXKZQLSBIH456CA"
-  secret_key = "notReal"
   region = "eu-west-1"
 }
 
@@ -8,7 +6,7 @@ resource "aws_instance" "niceT1" {
   ami           = "ami-0da83231e619d28c4"
   instance_type = "t2.micro"
    tags = {
-    Name = var.instance_name
+    Name = vr.instance_name
   }
 
 }
